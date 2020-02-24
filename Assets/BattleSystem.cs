3f5a28f50623e519a2f5a53aa61c0dd43aec9367
Busyakum_Andrew <busya117@hsd.k12.or.us>
2020-02-24 09:14:42 -0800

    Battle system script created

---
+++ Assets/BattleSystem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
+
+public class BattleSystem : MonoBehaviour
+{
+
+    public GameObject playerPrefab;
+    public GameObject enemyPrefab;
+
+    public Transform playerBattleStation;
+    public Transform enemyBattleStation;
+
+    public BattleState state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = BattleState.START;
+        SetupBattle();
+    }
+
+    void SetupBattle()
+    {
+        Instantiate(playerPrefab, playerBattleStation);
+        Instantiate(enemyPrefab, enemyBattleStation);
+    }
+
+  }
